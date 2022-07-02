5edd0404098016686f37b7f5db38a9f88e74f3d3
Shalhev Nagauker <shalhav96@gmail.com>
2022-07-02 20:21:30 +0300

    header css

--- js/cmps/header.cmp.js
+++ js/cmps/header.cmp.js
@@ -2,18 +2,22 @@ export default {
     template: `
     <section class="app-header">
         <div class="logo">
+            <a href="#">
             <span class="logo-blue">A</span>
             <span class="logo-red">p</span>
             <span class="logo-yellow">p</span>
             <span class="logo-blue">s</span>
             <span class="logo-green">u</span>
             <span class="logo-red">s</span>
+            </a>
             </div>
         <nav class="nav-bar">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/mail">Mail</router-link> |
-            <router-link to="/keep">Keep</router-link>
+            <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/mail">Mail</router-link></li>
+            <li><router-link to="/keep/notes">Keep</router-link></li>
             <!-- <router-link to="/books">Books</router-link> -->
+            </ul>
         </nav>
     </section>
 `,
